a4c9bd4f415d973aae795c6ace3765bddad580d4
Monica Arysta <monicaarystap@gmail.com>
2022-05-23 01:58:50 +0800

    Merevisi modul 1 hehe

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -43,6 +43,39 @@
                                 <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                             </ul>
                         </li>
+                        @if(!Auth::guard('web')->user()->email_verified_at)
+{{-- <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
+                <div class="card-body">
+                    @if (session('resent'))
+                        <div class="alert alert-success" role="alert">
+                            {{ __('A fresh verification link has been sent to your email address.') }}
+                        </div>
+                    @endif
+
+                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('If you did not receive the email') }},
+                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+                    <li class="nav-item">
+                        <form method="POST" action="{{ route('verification.resend') }}">
+                            @csrf
+                            <button class="nav-link btn btn-link">{{ __('Activate Email') }}</button>
+                        </form>
+                    </li>
+                    @endif
+
                     </ul>
                     <form class="d-flex">
                         <button class="btn btn-outline-dark" type="submit">
@@ -54,7 +87,7 @@
 
                     <form action="{{ route('login') }}" method="get">
                         @csrf
-                        <button class="ml-2 btn btn-outline-dark" type="submit">Login
+                        <button class="ml-2 btn btn-outline-dark" type="submit">Logins
                         </button>
                     </form>
 
@@ -66,6 +99,11 @@
                 </div>
             </div>
         </nav>
+        @if (session('resent'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ __('A fresh verification link has been sent to your email address.') }}
+        </div>
+        @endif
 
         @yield('content')
 
--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ use Illuminate\Support\Facades\Auth;
 //     return view('welcome');
 // });
 Auth::routes();
+Auth::routes(['verify' => true]);
+
 
 
 Route::middleware(['guest:web'])->group(function(){
@@ -39,20 +41,25 @@ Route::middleware(['guest:web'])->group(function(){
     route::post('logins_proses',[Login::class,'proses_login'])->name('login_proses');
 
     //
-    Route::get('/', [TransaksiController::class, 'landingPage'])->name('landing-page-user');
-    Route::get('/{id}/produk-page', [TransaksiController::class, 'produkPage'])->name('produk-page');
-    Route::get('/transaksi-page', [TransaksiController::class, 'transaksiPage'])->name('transaksi-page');
-    Route::get('/bukti-pembayaran-page', [TransaksiController::class, 'buktiPembayaranPage'])->name('bukti-pembayaran-page');
-    Route::get('/status-transaksi-page', [TransaksiController::class, 'statusTransaksiPage'])->name('status-transaksi-page');
-
-    Route::get('/cart-page', [CartController::class, 'cartPage'])->name('cart-page');
-    Route::post('/cart-insert', [CartController::class, 'cartInsert'])->name('cart-insert');
 });
 Route::middleware(['auth:web'])->group(function(){
+    // ROUTE YANG BISA DIAKSES SEMUA USER LOGIN - UDAH VERIFY ATAUPUN BELUM EMAILNYA
     Route::get('home',[HomeController::class, 'index'])->name('home');
-    Route::view('profile',[Login::class,'profile'])->name('profile');
-    Route::post('/logout',[Login::class,'logout'])->name('logout');
+    Route::get('/', [TransaksiController::class, 'landingPage'])->name('landing-page-user');
+    Route::get('/{id}/produk-page', [TransaksiController::class, 'produkPage'])->name('produk-page');
+    Route::middleware('email')->group(function(){
+        // EMAIL YANG SUDAH TERVERIFIKASI YANG BISA MENGAKSES URL/FITUR
+        Route::get('/transaksi-page', [TransaksiController::class, 'transaksiPage'])->name('transaksi-page');
+        Route::get('/bukti-pembayaran-page', [TransaksiController::class, 'buktiPembayaranPage'])->name('bukti-pembayaran-page');
+        Route::get('/status-transaksi-page', [TransaksiController::class, 'statusTransaksiPage'])->name('status-transaksi-page');
+
+        Route::get('/cart-page', [CartController::class, 'cartPage'])->name('cart-page');
+        Route::post('/cart-insert', [CartController::class, 'cartInsert'])->name('cart-insert');
+        Route::view('profile',[Login::class,'profile'])->name('profile');
+    });
     // route::get('toko',[Toko::class,'toko'])->name('home');
+    Route::post('/logout',[Login::class,'logout'])->name('logout');
+
 });
 
 Route::prefix('admin')->name('admin.')->group(function(){
